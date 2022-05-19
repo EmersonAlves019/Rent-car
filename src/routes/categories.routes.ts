cecb08fc04fceabaf5337535b1b13add55f71ec4
EmersonAlves <emersondasilva.alves99@gmail.com>
2022-05-19 02:06:58 -0300

    feat: unbinding route dependencies

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,17 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/categories", (req, res) => {
   const { name, description } = req.body;
 
-  const category = new Category();
-  console.log(category);
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
-  return res.status(201).json(categories);
+  return res.status(201).send();
 });
 
 export { categoriesRoutes };
